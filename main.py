9814320d4ceb9f3aa825b17480824ea8b2c4354f
mkguhan <mkguhan@gmail.com>
2020-08-18 21:29:58 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import pandas as pd
+import urllib
+
+def get_url(url):
+    #driver = webdriver.Chrome(executable_path="C:\\Users\\Admin\\PycharmProjects\\stockinnews\\chromedriver.exe")
+    #driver.get(url)
+    page_source = urllib.request.urlopen(url)
+    return page_source
+
+
+if __name__ == "__main__":
+    content = get_url("https://www.nseindia.com/market-data/live-equity-market")
+    content_html = BeautifulSoup(content, 'lxml')
+    print(content_html)
